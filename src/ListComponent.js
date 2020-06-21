4e351efc72e259d98e9d5e46bbc5ece792303da8
syed <sabtain.info@gmail.com>
2020-06-21 11:23:52 -0700

    function of delete is passed from parent component and used it here

---
+++ src/ListComponent.js
@@ -0,0 +1,27 @@
+import React,{useContext,useState,useReducer} from 'react';
+
+
+
+const ListComponent = props=>
+{ 
+ 
+
+
+    return(
+        <li className="list-group-item list-group-item-action" >
+                
+                   <div className="row ">
+                       <div className="col-md-6 text-left ">
+                       
+                       <span> <button className="badge badge-danger " id={props.id} onClick={props.clicked} > X </button> </span>
+                       <span>{props.items.amountdescription}</span>
+                       </div>
+                       <div className="col-md-6 text-right">
+                          {props.items.amount}
+                       </div>
+                   </div>
+                </li>
+    )
+}
+
+export default ListComponent;
