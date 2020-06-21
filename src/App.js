1efb517cb64040dbfb5c9bc7b35108cad5e9f805
syed <sabtain.info@gmail.com>
2020-06-21 11:18:19 -0700

    3 Child Component is added with Globaldataprovider(context.provider)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import HistoryComponent from './HistoryComponent'
+import FormCompnent from './FormComponent'
+import './bootstrap/bootstrap.min.css'
+import BalanceCOmponent from './BalanceComponent';
+import {Gloabldataprovider} from './DataContext'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    return (
+        <Gloabldataprovider>
+            <div className="container text-center  ">
+                <div className=" specfic shadow-lg">
+                    <div className="mt-5 font-weight-bolder  pt-1     rounded font-size col-md-12">Expense Tracker</div>
+                    <span className=" font-weight-bolder font-size-14">By Syed Sabtain Raza</span>
+                    <BalanceCOmponent></BalanceCOmponent>
+                    <h2 className="mt-5 ">History</h2>
+                    <hr/>
+                    <HistoryComponent></HistoryComponent>
+
+                    <h3 className="mt-5">Add new transaction</h3>
+                    <hr className="border shadow"/>
+                    <FormCompnent></FormCompnent>
+
+                </div>
+            </div>
+            </Gloabldataprovider>
+    )
 }
 
 export default App;
